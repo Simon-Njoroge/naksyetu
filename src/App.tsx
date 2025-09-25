7cb8f57c7dfcdcd076f173a1e558cb090050e3de
Simon-Njoroge <mukirisimon22@gmail.com>
2025-09-25 04:07:38 +0300

    initial

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import myImage from '/WhatsApp Image 2025-09-24 at 3.31.20 PM.jpeg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <img src={myImage} className="fullscreen-image" alt="My Image" />
+    </div>
+  )
+}
+
+export default App
